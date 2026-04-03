c7cc53b1f2d389e1194e71efba3314439bca3542
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-03 09:20:55 -0500

    prueba 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2s_v3 "
+    vm_size    = "Standard_DC2s_v3"
   }
   identity { type = "SystemAssigned" }
 }
